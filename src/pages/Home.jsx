01b4e0f93efbc28caafd103010a963552cfe81ce
Simon Tessard <simon.tessard@gmail.com>
2023-04-11 18:14:02 +0200

    First version of home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import ButtonCurrentEmployee from '../components/ButtonCurrentEmployee'
+
+function Home() {
+  return (
+    <main>
+      <ButtonCurrentEmployee />
+    </main>
+  )
+}
+
+export default Home

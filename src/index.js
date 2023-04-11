6989a23b86edff38d260423ba907f2d3e9d4ee26
Simon Tessard <simon.tessard@gmail.com>
2023-04-11 17:51:53 +0200

    Changed path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header/Header'
+import Header from './components/Header'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))

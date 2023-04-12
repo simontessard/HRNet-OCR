4ff1762d4af9a7e8fcca09d79b5edf15f485e28a
Simon Tessard <simon.tessard@gmail.com>
2023-04-12 19:21:46 +0200

    Add: Navlink to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,35 @@
 import styled from 'styled-components'
 import { FaUserTie } from 'react-icons/fa'
 
+import { NavLink } from 'react-router-dom'
+
 const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 3rem;
   padding: 1rem;
-  gap: 0.75rem;
 `
 
 const StyledHFaUserTie = styled(FaUserTie)`
   font-size: 2rem;
   background-color: #52c0db;
   border-radius: 25px;
+  margin-right: 0.75rem;
+`
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
 `
 
 function Header() {
   return (
     <StyledHeader>
-      <StyledHFaUserTie />
-      HRNet
+      <StyledNavLink to="/">
+        <StyledHFaUserTie />
+        HRNet
+      </StyledNavLink>
     </StyledHeader>
   )
 }
